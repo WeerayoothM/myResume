c4344f76fce3e20389cd92953ae620d14eedd015
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2020-09-18 21:08:13 +0700

    third commit

--- js/scripts.js
+++ js/scripts.js
@@ -105,8 +105,34 @@ $(window).on("load", function () {
   $(".loader-wrapper").fadeIn(800);
   $(".loader-wrapper").delay(1000).fadeOut(800);
 });
-// location.reload();
 
-// window.onload = function () {
-//   document.body.className += " loaded";
-// };
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-white";
+}
